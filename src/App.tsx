bc1093f14c54b9321f5731753535f46e768567ff
Erica <erica.friberg89@gmail.com>
2026-09-17 17:53:44 +0200

    Adjusting files for new structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 import UserContext from './contexts/AuthContext'
 import Login from './pages/Login'
 import { SiteFooter } from './components/SiteFooter'
@@ -12,20 +11,17 @@ function App() {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <main className='flex-1 flex py-10 w-full max-w-6xl mx-auto px-4'>
-          <SiteHeader />
-            <LandingPage />
-            <SiteFooter />
+        <SiteHeader />
+        
         <UserContext value={{ user, setUser }} >
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<div>Home</div>} />
+              <Route path="/" element={<LandingPage />} />
               <Route path="/login" element={<Login />} />
             </Routes>
           </BrowserRouter>
         </UserContext>
-      </main>
-
+        <SiteFooter />
     </div>
   )
 
