e465b299edabfa06fc9c343e4af96dbfc35f6ada
Viktor Lindqvist <lindqvistcs@gmail.com>
2026-09-24 13:17:10 +0200

    Added href to Button element to get to gift product page

--- src/components/GiftsCard.tsx
+++ src/components/GiftsCard.tsx
@@ -93,7 +93,7 @@ const GiftsCard = ({ userDetails, gift, memberships, category }: CardDetails) =>
             <div className="p-5 pt-0">
                 {available ?
                     pointsLeft >= 0 ?
-                        <Button className="w-full cursor-pointer">
+                        <Button href={`/gifts/${gift.id}`} className="w-full cursor-pointer">
                             <span>Välj denna gåva</span>
                             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
